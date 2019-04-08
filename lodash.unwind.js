38ba2d4e4a144575833edab68fbba9be297c825a
Giancarlo Palavicini <kasthor@gmail.com>
2019-04-08 17:10:07 -0600

    feat: mongo's $unwind functionality for lodash

---
+++ lodash.unwind.js
@@ -0,0 +1,26 @@
+const _ = require('lodash');
+
+module.exports = {
+  unwind(
+    obj,
+    key,
+    func = function (i, ii) {
+      return Object.assign({}, i, { [key]: ii });
+    },
+  ) {
+    const result = [];
+
+    _.each(obj, (item) => {
+      if (_.isArray(item[key])) {
+        _.each(_.get(item, key), (iitem) => {
+          result.push(func(_.omit(_.clone(item), key), iitem));
+        });
+      } else {
+        result.push(item);
+      }
+    });
+
+    return result;
+  },
+};
+
---
+++ test/lodash.unwind.spec.js
@@ -0,0 +1,44 @@
+/* global beforeAll test expect */
+
+const _ = require('lodash');
+
+_.mixin(require('../lodash.unwind'));
+
+let object;
+
+beforeAll(() => {
+  object = [
+    {
+      regular: 'key',
+      values: [{ l2: 1 }, { l2: 2 }, { l2: 3 }],
+    },
+  ];
+});
+
+test('return an array', () => {
+  const test = _.unwind(object, 'values');
+
+  expect(test).toBeInstanceOf(Array);
+});
+
+test('array has 3 elements', () => {
+  const test = _.unwind(object, 'values');
+
+  expect(test.length).toEqual(object[0].values.length);
+  test.forEach(i => expect(i.values).toBeInstanceOf(Object));
+});
+
+test('a function can modify the way items are merged', () => {
+  const test = _.unwind(object, 'values', _.merge);
+
+  test.forEach((i) => {
+    expect(i.l2).not.toBeNull();
+    expect(i.values).toBeUndefined();
+  });
+});
+
+test('a function can modify the way items are merged', () => {
+  const test = _.unwind(object, 'regular', _.merge);
+
+  expect(test.length).toEqual(1);
+});
